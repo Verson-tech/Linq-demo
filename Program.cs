305ccdc8e5932d16c2ea333c4e28e094ce36f047
Vira Murray <vira.avramenko@gmail.com>
2022-09-29 15:59:08 -0400

    Linq demo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+int[] array1 = { 098, 234, 456, 234, 765, 095,23,8, 8976, 234, 345 };
+
+  array1.Where(x => x < 100)
+        .ToList()
+        .ForEach(x=>Console.WriteLine(x));
+
+
+//foreach (var num in myNewCollection)
+//{
+//    Console.WriteLine(num);
+//}
+
+
+//for (int i = 0; i < myNewCollection.Count(); i++)
+//{
+//    Console.WriteLine(myNewCollection);
+//}
